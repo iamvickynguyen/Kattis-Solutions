22b7c2fe40d1c0940d32277df3e8d1cdf15dc06e
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-04 22:26:55 -0800

    solved

--- candle_box.py
+++ candle_box.py
@@ -1,5 +1,17 @@
-diff = int(input())
-rita = int(input())
-theo = int(input())
+d = int(input())
+r = int(input())
+t = int(input())
 
-# easy math, solve tomorrow. Now go to bed!
+# rita: [4 + ... + (2+d)] + (3+d) + (4+d) + ... + (i+d)
+# theo:                     3     + 4   + ... + i
+# sum of candles = [4 + ... + (2+d)] + (2*3+d + 2*4+d + ... + 2*i+d) = x + y
+x = sum(range(4, 3+d))
+i = 3
+y = 2*i+d
+while y < (r+t) - x:
+    i += 1
+    y += 2*i+d
+
+real_candles = sum(range(4, i+d+1))
+remove = r - real_candles
+print(remove)
