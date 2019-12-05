e32d1a90610172edda67f69533bf9da5ba108207
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-04 22:54:25 -0800

    solved

--- judging_troubles.py
+++ judging_troubles.py
@@ -1,15 +1,24 @@
-submissions = int(input())
-dict = {}
-for i in range(submissions*2):
-    key = input()
-    if key not in dict:
-        dict[key] = 1
+n = int(input())
+dom = {}
+for i in range(n):
+    judge = input()
+    if judge not in dom:
+        dom[judge] = 1
     else:
-        dict[key] += 1
+        dom[judge] += 1
 
+kattis = {}
+for i in range(n):
+    judge = input()
+    if judge not in kattis:
+        kattis[judge] = 1
+    else:
+        kattis[judge] += 1
+
+# compare 2 dictionaries, find match
 count = 0
-for val in dict.values():
-    if val > 1:
-        count += val//2
+for key in dom.keys():
+    if key in kattis:
+        count += min(dom[key], kattis[key])
 
 print(count)
