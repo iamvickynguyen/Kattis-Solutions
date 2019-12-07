7e9398641a4dfaac235cb5d602cac722d937b2b7
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-06 22:53:20 -0800

    solved

--- basic_remains.py
+++ basic_remains.py
@@ -1,13 +1,3 @@
-def to_base_10(num, b):
-    num = [int(x) for x in str(num)]
-    result = 0
-    x = len(num) - 1
-    for i in range(len(num)):
-        result += num[i]*(b**x)
-        x -= 1
-    return result
-
-
 def to_original_base(n, b):
     if n == 0:
         return 0
@@ -23,10 +13,9 @@ data = [int(x) for x in input().split()]
 
 while data[0] != 0:
     b = data[0]
-    # p = to_base_10(data[1], b)
-    # m = to_base_10(data[2], b)
-    # k = p % m
-    # print(to_original_base(k, b))
-    print(int('15', 2))
-
+    p = int(str(data[1]), b)
+    m = int(str(data[2]), b)
+    k = p % m
+    result = to_original_base(k, b)
+    print(result)
     data = [int(x) for x in input().split()]
