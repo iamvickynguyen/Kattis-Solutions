a63381b432bb575062a2b7a08d7edf4f5f6f40e5
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-08 20:54:43 -0800

    solved

--- tired_terry.py
+++ tired_terry.py
@@ -1,5 +1,23 @@
 data = [int(x) for x in input().split()]
+length = data[0]
 p = data[1]
 d = data[2]
 pattern = input()
-for i in range(len(pattern)):
+pattern += pattern
+w = 0
+z = 0
+count = 0
+for i in range(length*2):
+    if pattern[i] == 'W':
+        w += 1
+    else:
+        z += 1
+    if i-p >= 0:
+        if pattern[i-p] == 'W':
+            w -= 1
+        else:
+            z -= 1
+    if i>= length:
+        if z < d:
+            count += 1
+print(count)
