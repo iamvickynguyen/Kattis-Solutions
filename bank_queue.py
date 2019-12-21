91ae5c4349dfbe5f64f2931c0aaa2278739ccf3c
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-21 00:40:08 -0800

    solved

--- bank_queue.py
+++ bank_queue.py
@@ -1,44 +1,22 @@
-import sys
+# Idea: Greedy, similar job sequencing with deadlines
+info = [int(x) for x in input().split()]
+data = []
+for _ in range(info[0]):
+    data.append([int(x) for x in input().split()])
 
-# ============ TEST ===================
-# A Dynamic Programming based Python Program for 0-1 Knapsack problem 
-# Returns the maximum value that can be put in a knapsack of capacity W 
-def knapSack(W, wt, val, n): 
-    K = [[0 for x in range(W+1)] for x in range(n+1)] 
-  
-    # Build table K[][] in bottom up manner 
-    for i in range(n+1): 
-        for w in range(W+1): 
-            if i==0 or w==0: 
-                K[i][w] = 0
-            elif wt[i-1] <= w: 
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]) 
-            else: 
-                K[i][w] = K[i-1][w] 
-  
-    return K[n][W] 
+# sort money descending
+data.sort(key=lambda x: x[0], reverse=True)
 
-# idea: sort time ascending, take the max money if some customers leave at the same time
-line = [int(x) for x in input().split()]
-close = line[1]
-data = [None]*close
-for _ in range(line[0]):
-    temp = [int(x) for x in input().split()]
-    if temp[1] < close:
-        if data[temp[1]] == None:
-            data[temp[1]] = temp[0]
-        else:
-            data[temp[1]] = max(temp[0], data[temp[1]])
+# time slots
+time = info[1]
+slots = [False] * time
+total = 0
+for i in range(len(data)):
+    for j in range(min(time, data[i][1]), -1, -1):
+        # if exists free slot
+        if slots[j] is False:
+            slots[j] = True
+            total += data[i][0]
+            break
 
-
-val = []
-time = []
-for j in range(close):
-    if data[j] != None:
-        val.append(data[j])
-        time.append(j)
-
-n = len(val) 
-print(knapSack(close, time, val, n)) 
-           
-
+print(total)
