002446f5423e746a95323d40f5f8664ed51d73e4
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-23 01:30:55 -0800

    solved (common interview question)

--- palindromic_password.py
+++ palindromic_password.py
@@ -1,14 +1,47 @@
+# find lower and upper palindrome for 2 digits at index 2,3
+def find_2_digits_palindrome(n):
+    lower = n
+    while lower % 10 != lower//10:
+        lower -= 1
+    if lower == 0:
+        lower = "00"
+    upper = n
+    while upper % 10 != upper//10:
+        upper += 1
+    return str(lower), str(upper)
+
+
+def print_result(num1, num2, num):
+    if abs(num1 - int(num)) < abs(num2 - int(num)):
+        print(num1)
+    elif abs(num1 - int(num)) > abs(num2 - int(num)):
+        print(num2)
+    elif num1 < num2:
+        print(num1)
+    else:
+        print(num2)
+
+
 line = int(input())
 for i in range(line):
-    num = [x for x in input()]
-    front = ''
-    back = ''
-    while len(num) > 0:
-        if int(num[0]) > int(num[-1]) and int(num[-1]) != 0:
-            front += num[-1]
-            back = num[-1] + back
-        else:
-            front += num[0]
-            back = num[0] + back
-        num = num[1:-1]
-    print(front+back)
+    num = input()
+
+    # if number at index 2,3 are 99 or 00, increment by 1 or decrement by 1
+    if int(num[2:4]) == 99:
+        front = str(int(num[:3]) + 1)
+        num1 = int(front + front[::-1])
+        num2 = int(num[:3] + num[2::-1])
+        print_result(num1, num2, num)
+    elif int(num[2:4]) == 00:
+        front = str(int(num[:3]) - 1)
+        num1 = int(front + front[::-1])
+        num2 = int(num[:3] + num[2::-1])
+        print_result(num1, num2, num)
+
+    # else find lower and upper palindromic number for number at index 2,3
+    # then compare
+    else:
+        lower, upper = find_2_digits_palindrome(int(num[2:4]))
+        num1 = int(num[:2] + lower + num[1::-1])
+        num2 = int(num[:2] + upper + num[1::-1])
+        print_result(num1, num2, num)
