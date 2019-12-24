a7d7404109ad85a2e6bc1ff932e42361b53c665e
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2019-12-24 11:58:13 -0800

    solved

--- adding_words.py
+++ adding_words.py
@@ -1,5 +1,4 @@
 import sys
-import math
 
 def_val = {}
 val_def = {}
@@ -7,32 +6,33 @@ val_def = {}
 for line in sys.stdin:
     line = line.strip()
     n = line.split()
-    key = [n[x] for x in range(1, len(n)) if x % 2 != 0]
+    key = [n[x] for x in range(len(n)) if x % 2 != 0]
     if n[0] == 'def':
+        if n[1] in def_val:
+            del(val_def[def_val[n[1]]])
         def_val[n[1]] = n[2]
         val_def[n[2]] = n[1]
     elif n[0] == 'calc':
+        # if definition is not defined
         for item in key:
             if item not in def_val:
                 print(line[5:] + " unknown")
                 break
+
+        # definition is defined, check if result is defined or not
         else:
-            s = ''
+            equation = ''
             for i in range(1, len(n)-1):
                 if n[i] in def_val:
-                    s += def_val[n[i]]
+                    equation += def_val[n[i]]
                 else:
-                    s += n[i]
-            if len(s) > 1:
-                result = str(eval(s))
-                if result not in val_def:
-                    print(line[5:] + " unknown")
-                else:
-                    print(line[5:] + " " + val_def[result])
+                    equation += n[i]
+
+            result = str(eval(equation))
+            if result in val_def:
+                print(line[5:] + " " + val_def[result])
             else:
-                if key[0] not in def_val:
-                    print(line[5:] + " unknown")
-                # print(line[5:] + " " + key[0])
+                print(line[5:] + " unknown")
     else:
         def_val.clear()
         val_def.clear()
