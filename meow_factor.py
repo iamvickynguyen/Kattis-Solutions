9d0930943b58a041397ca162885e66b7e3e97442
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-07 00:19:43 -0800

    solved

--- meow_factor.py
+++ meow_factor.py
@@ -1,9 +1,8 @@
 import math
-import sys
 
 n = int(input())
-# mathematically, math.floor makes more sense, but n**(1/9) doesn't give precise answer (ie: 12 becomes 11.9999999999)
 x = math.ceil(n ** (1/9))
+# mathematically, math.floor makes more sense, but n**(1/9) doesn't give precise answer (ie: 12 becomes 11.9999999999)
 while n % x ** 9 != 0:  # note: math.pow(x, 9) doesn't give precise answer
     x -= 1
 print(x)
