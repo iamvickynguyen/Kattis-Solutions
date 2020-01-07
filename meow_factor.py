c9c028244ddc1bffb0eb0efececedfe27a3c5eb7
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-07 00:18:36 -0800

    solved

--- meow_factor.py
+++ meow_factor.py
@@ -1,34 +1,9 @@
 import math
-
-
-def PrimeFactor(n):
-    power = {}
-    while not n % 2:
-        try:
-            power[2] += 1
-        except KeyError:
-            power[2] = 1
-        n //= 2
-    i = 3
-    while i * i <= n:
-        while not n % i:
-            try:
-                power[i] += 1
-            except KeyError:
-                power[i] = 1
-            n //= i
-        i += 2
-    if n != 1:
-        power[n] = 1
-    return power
-
+import sys
 
 n = int(input())
-result = 1
-factors = PrimeFactor(n)
-for k, v in factors.items():
-    if v >= 9:
-        x = v//9
-        result *= math.pow(k, x)
-
-print(int(result))
+# mathematically, math.floor makes more sense, but n**(1/9) doesn't give precise answer (ie: 12 becomes 11.9999999999)
+x = math.ceil(n ** (1/9))
+while n % x ** 9 != 0:  # note: math.pow(x, 9) doesn't give precise answer
+    x -= 1
+print(x)
