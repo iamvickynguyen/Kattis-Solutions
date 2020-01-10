731f0f12ebf588812b3acb9a58f2ddb7950cc33a
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-09 22:37:39 -0800

    solved

--- factstone_benchmark.py
+++ factstone_benchmark.py
@@ -1,11 +1,14 @@
+# Idea: 2**bit >= n! -> bit >= log(n!)/log(2)
+# log(n!) = log(1) + log(2) + ... + log(n)
+import math
 n = int(input())
 while n != 0:
     bit = 4 * (2**((n-1960)//10))
-    num = 2**bit
-    val = 1
-    for i in range(1, num):
-        val *= i
-        if val >= num:
-            print(i-1)
-            break
+    i = 1
+    val = math.log(i)/math.log(2)
+    while bit >= val:
+        bit -= val
+        i += 1
+        val = math.log(i)/math.log(2)
+    print(i-1)
     n = int(input())
