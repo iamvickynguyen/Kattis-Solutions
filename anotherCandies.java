ca62f98025283c3aaf82c275f8304963fead1f1e
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-11 20:43:15 -0800

    solved

--- anotherCandies.java
+++ anotherCandies.java
@@ -1,19 +1,19 @@
-import java.math.BigInteger;
+
+//import java.math.BigInteger;
 import java.util.Scanner;
 
 class anotherCandies {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int numLine = scanner.nextInt();
 		for (int i = 0; i < numLine; i++) {
-			BigInteger sumCandies = new BigInteger("0");
-			BigInteger numChildren = scanner.nextBigInteger();
-			for (int j = 0; j < numChildren.intValue(); j++) {
-				BigInteger tmp = scanner.nextBigInteger();
-				sumCandies = ((tmp.mod(numChildren)).add(sumCandies.mod(numChildren))).mod(numChildren);
+			long sumCandies = 0;
+			long numChildren = scanner.nextLong();
+			for (int j = 0; j < numChildren; j++) {
+				long tmp = scanner.nextLong();
+				sumCandies = ((tmp % numChildren) + (sumCandies % numChildren)) % numChildren;
 			}
-			if (sumCandies.intValue() == 0) {
+			if (sumCandies == 0) {
 				System.out.println("YES");
 			} else {
 				System.out.println("NO");
@@ -22,4 +22,15 @@ class anotherCandies {
 		scanner.close();
 	}
 
+	/*
+	 * public static void main(String[] args) { Scanner scanner = new
+	 * Scanner(System.in); int numLine = scanner.nextInt(); for (int i = 0; i <
+	 * numLine; i++) { BigInteger sumCandies = new BigInteger("0"); BigInteger
+	 * numChildren = scanner.nextBigInteger(); for (int j = 0; j <
+	 * numChildren.intValue(); j++) { BigInteger tmp = scanner.nextBigInteger();
+	 * sumCandies =
+	 * ((tmp.mod(numChildren)).add(sumCandies.mod(numChildren))).mod(numChildren); }
+	 * if (sumCandies.intValue() == 0) { System.out.println("YES"); } else {
+	 * System.out.println("NO"); } } scanner.close(); }
+	 */
 }
