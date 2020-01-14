20268ea1439e02b72ae278f6b98c8fa79b4a5050
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-13 23:48:40 -0800

    solved

--- long_swaps.py
+++ long_swaps.py
@@ -1,34 +1,21 @@
 import math
 import sys
 data = input().split()
-# n = len(data[0])//2
-# if len(data[0]) % 2 ==1:
-#     real = sorted(data[0])
-#     if data[0][n] == real[n]:
-#         print("Yes") if int(data[1]) <= n+1 else print("No")
-#         sys.exit(0)
-# print("Yes") if int(data[1]) <= n else print("No")
-
-# compare
-total_error = 0
-error = {}
-s = sorted(data[0])
-for i in range(len(s)):
-    if s[i] not in error:     
-        error[s[i]] = [i]
-    else:
-        error[s[i]].append(i)
-
-for i in range(len(s)):
-    total_error += abs(error[data[0][i]].pop(0) - i)
-
-print(total_error)
-
+s = data[0]
 k = int(data[1])
-mid = len(s)//2
-if len(data[0]) % 2 ==1:
-    if data[0][mid] == s[mid]:
-        if k <= mid + 1 and (total_error + 1) % k == 0:
-            print("Yes")
-            sys.exit(0)
-print("Yes") if total_error % k == 0 and k <= mid+1 else print("No")
+s_sort = ''.join(sorted(s))
+if s_sort == s:
+    print("Yes")
+elif k >= len(s):
+    print("No")
+elif k > math.ceil(len(s)/2):
+    if s_sort[(len(s)-k): k] == s[(len(s)-k): k]:
+        print("Yes")
+    else:
+        print("No")
+elif len(s) - k - k + 2 >= k:
+    print("Yes")
+elif len(s) - k - k + 2 == 1 and s_sort[k - 1: (len(s)-k + 1)] != s[k - 1: (len(s)-k + 1)]:
+    print("No")
+else:
+    print("Yes")
