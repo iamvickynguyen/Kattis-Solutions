3ce12f87ef222bd0f8e58fb51f5e194711f5c30a
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-22 00:02:55 -0800

    need to check the required output

--- polish_notation.py
+++ polish_notation.py
@@ -1,27 +1,25 @@
 import sys
-
+case = 1
 ops = set(['+', '-', '*'])
 for line in sys.stdin:
     line = line.strip().split()
     stack = []
     s = ''
     for i in range(len(line)):
-        if line[i] in ops:
+        if not line[i].isdigit():
             stack.append(line[i])
-        elif line[i].isalpha():
-            s = line[i] + s
-            while len(stack) > 0:
-                s = stack.pop() + str(s)
         elif line[i].isdigit():
-            if len(stack) > 0:
-                while stack[-1].isdigit():
+            if len(stack) > 1:
+                while len(stack) > 1 and stack[-1].isdigit() and stack[-2] in ops:
                     a = stack.pop()
                     op = stack.pop()
                     cal = a + op + line[i]
-                    result = eval(cal)
-                    s = result + s
+                    line[i] = str(eval(cal))
                 stack.append(line[i])
-            while len(stack) > 0:
-                s = stack.pop() + str(s)
+            else:
+                stack.append(line[i])
 
-    print(s)
+    while len(stack) > 0:
+        s = stack.pop() + ' ' + s
+    print('Case ' + str(case) + ': ' + s)
+    case += 1
