c53af9cacca87a0e4dd4f2290d68d77a59057288
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-23 21:39:28 -0800

    solved

--- squawk_virus.py
+++ squawk_virus.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 data = [int(x) for x in input().split()]
 graph = [[] for i in range(data[0])]
 for _ in range(data[1]):
@@ -7,22 +5,36 @@ for _ in range(data[1]):
     graph[int(ip[0])].append(int(ip[1]))
     graph[int(ip[1])].append(int(ip[0]))
 
-# add all neighbors of s to queue, and increment time t
-q = deque()
-count = 0
-t = 0
-user = {i: 0 for i in range(data[0])}
-q.append([data[2], 0])
-user[data[2]] = 1
-while q and t < data[3]:
-    v, t = q.popleft()
-    for u in graph[v]:
-        # if user[u] == 0:
-        #     q.append([u, t+1])
-        q.append([u, t+1])
-        user[u] += user[v]
-        if t + 1 == data[3]:
-            count += user[v]
-    user[v] = 0
+# use 2 dictionaries
+# add all neighbours to the other dictionary, clear the current
+# switch dictionary as we go
+# if meet a repeated key, increment the value
+t = data[3]
+d1 = {}
+d2 = {}
+d1[data[2]] = 1
+while t > 0:
+    if len(d1) > 0:
+        for k, v in d1.items():
+            for u in graph[k]:
+                if u in d2:
+                    d2[u] += v
+                else:
+                    d2[u] = v
+        d1.clear()
+    else:
+        for k, v in d2.items():
+            for u in graph[k]:
+                if u in d1:
+                    d1[u] += v
+                else:
+                    d1[u] = v
+        d2.clear()
+    t -= 1
 
-print(count)
+if len(d1) > 0:
+    print(sum(d1.values()))
+elif len(d2) > 0:
+    print(sum(d2.values()))
+else:
+    print(0)
