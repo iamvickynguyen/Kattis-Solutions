660fea16c9dd3ad6429206ef531d34b747f820ae
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-25 12:43:10 -0800

    correct approach, consider cd.c file

--- cd.py
+++ cd.py
@@ -1,41 +1,49 @@
+# Note: since lists are in order, add everything to the first list, while reading the second list inputs, compare with the first list
+# This approach is more optimal than puting Jack's cds into a hash set, then checking if Jill's cd is in the set
+# However, Python is slow so you still get "time limit exceeded"
+# You can take a look at cd.c, which passed all test cases. 
+data = input()
+while data != "0 0":
+    n, m = map(int, data.split())
+    count = 0
+    l = []
+    for i in range(n):
+        l.append(int(input()))
+
+    i1 = 0
+    i2 = m
+    while i1 < n and i2 > 0:
+        x = int(input())
+        while i1 < n and l[i1] < x:
+            i1 += 1
+        if i1 >= int(data[0]):
+            break
+        if l[i1] == x:
+            count += 1
+            i1 += 1
+            i2 -= 1
+        elif l[i1] > x:
+            i2 -= 1
+
+    while i2 > 1:
+        x = input()
+        i2 -= 1
+
+    print(count)
+    data = input()
+
+
 # data = input()
 # while data != "0 0":
-#     data = data.split()
-#     count = 0
-#     l = []
-#     for i in range(int(data[0])):
-#         l.append(int(input()))
+	# n, m = map(int, data.split())
+	# count = 0
+	# cd = set()
+    # for i in range(n):
+        # cd.add(int(input()))
 
-#     i1 = 0
-#     i2 = int(data[1])
-#     while i1 < int(data[0]) and i2 > 0:
-#         x = int(input())
-#         while i1 < int(data[0]) and l[i1] < x:
-#             i1 += 1
-#         if i1 >= int(data[0]):
-#             break
-#         if l[i1] == x:
-#             count += 1
-#             i1 += 1
-#             i2 -= 1
-#         elif l[i1] > x:
-#             i2 -= 1
+    # for j in range(m):
+        # if int(input()) in cd:
+            # count += 1
 
-#     while i2 > 1:
-#         x = input()
-#         i2 -= 1
-
-#     print(count)
-#     data = input()
-
-
-while True:
-    n, m = map(int, input().split())
-    if n == 0 and m == 0:
-        break
-    a = set(int(input()) for _ in range(n))
-    count = 0
-    for _ in range(m):
-        if int(input()) in a:
-            count += 1
-    print(count)
+    # print(count)
+    # data = input()
