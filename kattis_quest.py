b7a616edc058dbb39e8549889a22d6d54805f14e
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-01-30 00:25:30 -0800

    still slow. Based on solution distribution on Kattis, guess I need to implement this in Java

--- kattis_quest.py
+++ kattis_quest.py
@@ -85,10 +85,78 @@
 # ====================sol 2=========================================
 
 # ========================= SOL 3 =======================
-import heapq
+# import heapq
 
+# q = []
+# removed = {}
+# line = int(input())
+# for _ in range(line):
+#     ip = input().split()
+#     if ip[0] == 'add':
+#         heapq.heappush(q, (-int(ip[1]), -int(ip[2])))  # max heap
+#     else:
+#         x = int(ip[1])
+#         total = 0
+#         tmp = q.copy()
+#         e_tmp, g_tmp = -1, -1
+#         e_add, g_add = -1, -1
+#         while x > 0 and len(tmp) > 0:
+#             e, g = map(int, heapq.heappop(tmp))
+#             if (e, g) == (e_tmp, g_tmp):
+#                 if (e_add, g_add) == (e, g):
+#                     e_add, g_add = -1, -1
+#                     removed_times = 0
+#                 else:
+#                     removed_times -= 1
+#             else:
+#                 removed_times = 0 if (e, g) not in removed else removed[(e, g)]
+#             if -e <= x:
+#                 while removed_times > 0:
+#                     if len(tmp) <= 0:  # last item
+#                         e = -x - 1  # not to add to total
+#                         break
+#                     e, g = map(int, heapq.heappop(tmp))
+#                     removed_times -= 1
+#             if -e <= x and (((e, g) != (e_tmp, g_tmp) and (e, g) not in removed) or ((e, g) == (e_tmp, g_tmp))) and removed_times == 0:
+#                 total -= g
+#                 x += e
+#                 if (e, g) in removed:
+#                     removed[(e, g)] += 1
+#                 else:
+#                     removed[(e, g)] = 1
+#                     e_add, g_add = e, g
+#             e_tmp, g_tmp = e, g
+#             # else -e > x:
+#             # do nothing
+
+#         print(total)
+# ========================= SOL 4 =======================
+# import heapq
+
+# q = []
+# line = int(input())
+# for _ in range(line):
+#     ip = input().split()
+#     if ip[0] == 'add':
+#         heapq.heappush(q, (-int(ip[1]), -int(ip[2])))  # max heap
+#     else:
+#         x = int(ip[1])
+#         total = 0
+#         tmp = []
+#         while x > 0 and len(q) > 0:
+#             e, g = map(int, heapq.heappop(q))
+#             if -e <= x:
+#                 total -= g
+#                 x += e
+#             else:
+#                 tmp.append((e, g))
+#         print(total)
+#         q = tmp + q
+
+# ========================= SOL 4 =======================
+import heapq
+import itertools
 q = []
-removed = {}
 line = int(input())
 for _ in range(line):
     ip = input().split()
@@ -97,21 +165,13 @@ for _ in range(line):
     else:
         x = int(ip[1])
         total = 0
-        tmp = q.copy()
-        while x > 0 and len(tmp) > 0:
-            e, g = map(int, heapq.heappop(tmp))
-            removed_times = 0 if (e, g) not in removed else removed[(e, g)]
-            while removed_times > 0:
-                e, g = map(int, heapq.heappop(tmp))
-                removed_times -= 1
+        tmp = []
+        while x > 0 and len(list(q)) > 0:
+            e, g = map(int, heapq.heappop(q))
             if -e <= x:
                 total -= g
                 x += e
-                if (e, g) in removed:
-                    removed[(e, g)] += 1
-                else:
-                    removed[(e, g)] = 1
-            # else -e > x:
-                # do nothing
-
+            else:
+                tmp.append((e, g))
         print(total)
+        q[0:0] = tmp
