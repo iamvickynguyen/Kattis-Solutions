cd3b5db09184722e77091d47083bc4fa420db10a
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-02-08 00:32:05 -0800

    solved

--- guessing_game.py
+++ guessing_game.py
@@ -1,28 +1,18 @@
-low = 1
-high = 10
+low = -1
+high = 11
 n = int(input())
-numbers = [False]*10
 while n != 0:
     response = input()
     if response == 'too high':
-        if numbers[n-1] or low > high or n > high or n < low:
-            print("Stan is dishonest")
-        else:
-            numbers[n-1] = True
-            high = n - 1
+        high = min(high, n)
     elif response == 'too low':
-        if numbers[n-1] or low > high or n < low or n > high:
-            print("Stan is dishonest")
-        else:
-            numbers[n-1] = True
-            low = n + 1
+        low = max(low, n)
     else:
-        if numbers[n-1] or low > high or n < low or n > high:
-            print("Stan is dishonest")
-        else:
+        if low < high and n > low and n < high:
             print("Stan may be honest")
-        low = 1
-        high = 10
-        numbers = [False]*10
+        else:
+            print("Stan is dishonest")
+        low = -1
+        high = 11
 
     n = int(input())
