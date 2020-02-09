0391508a0d4410dee04a840820e02871507f7d4a
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-02-08 20:22:31 -0800

    solved

--- recenice.py
+++ recenice.py
@@ -1,13 +1,17 @@
-num_string = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six',
+num_string = {0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six',
               7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve',
               13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen',
-              17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty',
-              70: 'seventy', 80: 'eighty', 90: 'ninety', 100: 'onehundred', 200: 'twohundred', 300: 'threehundred', 400: 'fourhundred', 500: 'fivehundred', 600: 'sixhundred', 700: 'sevenhundred', 800: 'eighthundred', 900: 'ninehundred'}
+              17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty',
+              40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety',
+              100: 'onehundred', 200: 'twohundred', 300: 'threehundred', 400: 'fourhundred',
+              500: 'fivehundred', 600: 'sixhundred', 700: 'sevenhundred', 800: 'eighthundred', 900: 'ninehundred'}
 
-string_num = {v: k for k, v in num_string.items()}
 
-
-def to_string(count):
+def number_to_string(count):
+    if count == 0:
+        return 'four'
+    if count in num_string:
+        return num_string[count]
     s = ''
     if count > 99:
         hundred = count - count % 100
@@ -30,14 +34,16 @@ for i in range(line):
         s[i] = word
         count += len(word)
 
-# map
-if count > 99:
+# find $
+true_count = count
+cur_string_length = number_to_string(true_count)
+while len(cur_string_length) + count != true_count:
+    true_count += 1
+    cur_string_length = number_to_string(true_count)
 
-
-blank = to_string(count)
-
+# to string
 for x in range(line):
     if s[x] == None:
-        s[x] = blank
+        s[x] = cur_string_length
 
 print(" ".join(s))
