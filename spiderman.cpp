312e152aa707a6e24599a4870025363fe946b7a8
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-10-30 15:08:56 -0700

    solved

--- spiderman.cpp
+++ spiderman.cpp
@@ -1,76 +1,75 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
 using namespace std;
-
-vector<string> moves;
-
-void get_moves(vector<int> distance, int length, int position, int cur_dist, char direction, vector<char> move) {
-	move.push_back(direction);
-
-	if (cur_dist < 0 || position > length - 1) {
-		return;
-	}
-	if (cur_dist < 0 || (position == length - 1 && cur_dist)) {
-		return;
-	}
-
-	if (position == length - 1 && !cur_dist) {
-		string s = "";
-		for (auto c : move) {
-			s += c;
-		}
-		moves.push_back(s);
-		return;
-	}
-
-	get_moves(distance, length, position + 1, cur_dist + distance[position + 1], 'U', move);
-	get_moves(distance, length, position + 1, cur_dist - distance[position + 1], 'D', move);
-}
-
-int get_max_height(vector<int> dist, string move) {
-	int max_height = dist[0];
-	int cur_height = dist[0];
-	for (int i = 1; i < dist.size(); i++) {
-		cur_height += move[i] == 'U' ? dist[i] : (-dist[i]);
-		max_height = max(max_height, cur_height);
-	}
-	return max_height;
-}
-
-string get_opt_move(vector<int> dist) {
-	if (!moves.size())
-		return "IMPOSSIBLE";
-
-	string result = moves[0];
-	int lowest = 1000;
-	for (auto s : moves) {
-		int max_height = get_max_height(dist, s);
-		if (lowest > max_height) {
-			lowest = max_height;
-			result = s;
-		}
-	}
-	moves.clear();
-	return result;
-}
+const int INF = 9999;
 
 int main() {
 	int n, m, x;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &m);
-		if (m) {
-			vector<int> distance;
-			for (int j = 0; j < m; j++) {
-				scanf("%d", &x);
-				distance.push_back(x);
+	cin >> n;
+	while (n--) {
+		cin >> m;
+		vector<int> arr;
+		int sum = 0;
+		for (int i = 0; i < m ; i++) {
+			cin >> x;
+			sum += x;
+			arr.push_back(x);
+		}
+
+		vector<vector<int>> opt_height(m, vector<int>(sum + 1, INF));
+		vector<vector<int>> decision(m, vector<int>(sum + 1, 0));
+		opt_height[0][arr[0]] = arr[0]; // up
+		decision[0][arr[0]] = 1; // 1: up, -1: down
+
+		for (int r = 1; r < m; r++) {
+			for (int h = 0; h < sum + 1; h++) {
+				if (opt_height[r - 1][h] != INF) {
+
+					// down
+					if (h >= arr[r]) {
+						if (h - arr[r] < opt_height[r][h - arr[r]]) {
+							if (opt_height[r][h - arr[r]] >= opt_height[r - 1][h]) {
+								opt_height[r][h - arr[r]] = opt_height[r - 1][h];
+								decision[r][h - arr[r]] = -1;
+							}
+						}
+					}
+
+					// up
+					if (h + arr[r] < opt_height[r][h + arr[r]]) {
+						int k = max(h + arr[r], opt_height[r - 1][h]);
+						if (opt_height[r][h + arr[r]] >= k) {
+							opt_height[r][h + arr[r]] = k;
+							decision[r][h + arr[r]] = 1;
+						}
+					}
+				}
 			}
-			vector<char> move;
-			get_moves(distance, m, 0, distance[0], 'U', move);
-			string opt = get_opt_move(distance);
-			printf("%s\n", opt.c_str());
+		}
+
+		if (decision[m - 1][0] != -1) {
+			cout << "IMPOSSIBLE" << endl;
+		}
+		else {
+			int h = 0;
+			string s = "";
+			for (int i = m - 1; i >= 0; i--) {
+				if (decision[i][h] == 1) {
+					h -= arr[i];
+					s += "U";
+				}
+				else {
+					h += arr[i];
+					s += "D";
+				}
+			}
+
+			for (int i = m - 1; i >= 0; i--) {
+				cout << s[i];
+			}
+			cout << endl;
 		}
 	}
 
