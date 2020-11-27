720cd5f494d66519960f90270cea9021019d7a03
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-11-27 01:46:23 -0800

    solved

---
+++ carrots.py
@@ -0,0 +1,4 @@
+n, p = map(int, input().split())
+for i in range(n):
+    input()
+print(p)
---
+++ everywhere.py
@@ -0,0 +1,7 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = set()
+    for j in range(n):
+        s.add(input())
+    print(len(s))
