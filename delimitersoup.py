f0ba2cf35784eb07485360f4a83fea7eed5db422
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-12-05 01:14:01 -0800

    solved

--- delimitersoup.py
+++ delimitersoup.py
@@ -6,13 +6,8 @@ for i in range(l):
     if s[i] in ['(', '[', '{']:
         stack.append(s[i])
     elif s[i] != ' ':
-        if len(stack) > 0:
-            if (stack[-1] == '(' and s[i] == ')') or (stack[-1] == '[' and s[i] == ']') or (stack[-1] == '{' and s[i] == '}'):
-                stack.pop()
-            else:
-                print(s[i] + ' ' + str(i))
-                ok = False
-                break
+        if (len(stack) > 0) and ((stack[-1] == '(' and s[i] == ')') or (stack[-1] == '[' and s[i] == ']') or (stack[-1] == '{' and s[i] == '}')):
+            stack.pop()
         else:
             print(s[i] + ' ' + str(i))
             ok = False
