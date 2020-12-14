d7600b5d911af07cc624234b72149179924317ad
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-12-14 01:50:03 -0800

    solved

--- classy.py
+++ classy.py
@@ -1,46 +1,19 @@
-def convert_string_reverse(s):
-    result = ''
-    l = s.split('-')
-    for i in range(len(l)-1, -1, -1):
-        if l[i] == 'upper':
-            result += 'u'
-        elif l[i] == 'middle':
-            result += 'm'
-        else:
-            result += 'l'
-    return result
+class Person:
+    def __init__ (self, name, _class):
+        self.name = name
+        self._class = _class
 
-case = int(input())
-for _ in range(case):
+    def __lt__ (self, other):
+        return self.name < other.name if self._class == other._class else self._class > other._class
+
+t = int(input())
+for _ in range(t):
     people = []
     n = int(input())
     for i in range(n):
-        ip = input().split(':')
-        converted_string = convert_string_reverse(ip[1][1:-len(' class')])
-        people.append([ip[0], converted_string])
-    sort_classes = sorted(people, key=lambda x: x[1])
+        person = input().split()
+        p = person[1].split('-')[::-1]
+        people.append(Person(person[0][:-1], '-'.join(p + ['middle'] * (10 - len(p)))))
 
-    # sort by alphabet
-    # iterate, if previous length == current length, check the last char
-    # if same, continue; otherwise, sort the current list
-    output = []
-    if len(sort_classes) > 0:
-        cur_class = sort_classes[0][1]
-        cur_length = len(cur_class)
-        current_list = [sort_classes[0][0]]
-        for index in range(1, n):
-            [person, status] = sort_classes[index]
-            if len(status) <= cur_length or status > cur_class:
-                # sort
-                current_list.sort(key=lambda x: x[0], reverse = True)
-                output += current_list
-                current_list.clear()
-            current_list.append(person)
-            cur_length = len(status)
-            cur_class = status
-        current_list.sort(key=lambda x: x[0], reverse = True)
-        output += current_list
-    for item in output[::-1]:
-        print(item)
-    print('==============================')
-
+    print('\n'.join(p.name for p in sorted(people)))
+    print('=' * 30)
