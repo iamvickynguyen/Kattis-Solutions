4fb9c3ea7d93140472b2252c36250968b4f1cdf3
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2021-11-16 21:28:31 -0800

    solved

---
+++ islandhopping.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef priority_queue<int, vector<int>, greater<int>> minpq;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, vi> ivi;
+typedef unordered_map<int, int> ii;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+class UF {
+	private:
+		vi parent, rank;
+	public:
+		UF(int N) {
+			parent.assign(N, 0);
+			for (int i = 0; i < N; i++) parent[i] = i;
+			rank.assign(N, 0);
+		}
+
+		int find(int v) {
+			if (parent[v] != v) parent[v] = find(parent[v]);
+			return parent[v];
+		}
+
+		bool same_set(int a, int b) {
+			return find(a) == find(b);
+		}
+
+		void union_set(int a, int b) {
+			int ra = find(a), rb = find(b);
+			if (ra == rb) return;
+			if (rank[ra] < rank[rb]) swap(ra, rb);
+			parent[rb] = ra;
+			if (rank[ra] == rank[rb]) rank[ra]++;
+		}
+};
+
+double kruskal(vector<tuple<double, int, int>> &graph, int n) {
+	sort(all(graph));
+	UF uf(n);
+
+	double cost = 0;
+	int cnt = 0;
+	for (int i = 0; i < graph.size(); i++) {
+		double w; int u, v;
+		tie (w, u, v) = graph[i];
+		if (!uf.same_set(u, v)) {
+			cost += w;
+			uf.union_set(u, v);
+			cnt++;
+
+			if (cnt == n - 1) break;
+		}
+	}
+	return cost;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	cin >> n;
+	double x, y;
+	while (n--) {
+		cin >> m;
+		vector<pair<double, double>> v;
+		for (int i = 0; i < m; i++) {
+			cin >> x >> y;
+			v.pb({x, y});
+		}
+
+		vector<tuple<double, int, int>> graph;
+		for (int i = 0; i < m; i++) {
+			for (int j = i + 1; j < m; j++) {
+				double weight = sqrt(pow(v[i].first - v[j].first, 2) + pow(v[i].second - v[j].second, 2));
+				graph.pb({weight, i, j});
+			}
+		}
+
+		cout << kruskal(graph, m) << '\n';
+	}
+
+	return 0;
+}
