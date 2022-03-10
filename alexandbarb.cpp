7c6039743813d37f55df9d192e3d720beae9ebdc
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-03-09 21:09:31 -0800

    solved

---
+++ alexandbarb.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, vi> ivi;
+typedef unordered_map<int, int> ii;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int k, m, n;
+	cin >> k >> n >> m;
+	int r = k % (n + m);
+	if (r < n) cout << "Barb";
+	else cout << "Alex";
+
+	return 0;
+}
